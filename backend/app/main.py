e3bee28b12483c4a5206064ade159287a51d2217
Altheo Saquilayan <altheo.saquilayan@exist.com>
2024-12-05 23:49:15 +0800

    SQLEXEC-7: Connect docker-compose of nlp-modules and core-modules together

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from pymongo.mongo_client import MongoClient
 from config import settings  # Importing directly, as config.py is in the same directory
 
 app = FastAPI(
@@ -6,6 +7,18 @@ app = FastAPI(
     version=settings.APP_VERSION
 )
 
+client = MongoClient(settings.mongodb_uri)
+
 @app.get("/")
 def health_check():
-    return {"status": f"{settings.APP_NAME} is running in {settings.APP_ENV} mode"}
+    try:
+        client.admin.command('ping')
+        return {
+            "backend_status": f"{settings.APP_NAME} is running in {settings.APP_ENV} mode",
+            "mongodb_status": f"SQLExecutor is connected to MongoDB"
+        }
+    except Exception as e:
+        print(e)
+        return{
+            "SQLExecutor Application Healthcheck failed"
+        }
