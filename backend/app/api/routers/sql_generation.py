1383cbab7448d3a88da17406b69efcb5713b5b45
Altheo Saquilayan <altheo.saquilayan@exist.com>
2024-12-21 16:33:28 +0800

    feature/SQLEXEC-26

--- backend/app/api/routers/sql_generation.py
+++ backend/app/api/routers/sql_generation.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Depends, HTTPException
 
 from api.core.services.llm_wrapper.llm_service_wrapper import LLMServiceWrapper
 from api.core.services.tenant_manager.tenant_manager_service import TenantManagerService
+from api.core.services.schema.schema_manager_service import SchemaManagerService
 from api.core.services.ruleset.ruleset_manager_service import RulesetManagerService
 from api.core.resolvers.query_scope.query_scope_resolver import QueryScopeResolver
 from api.core.resolvers.access_control.user_access_control_resolver import AccessControlResolver
@@ -45,3 +46,30 @@ async def generate_sql(tenant_id: str, user_request: UserInputRequest, session: 
     return resolved_user_query_scope
     # return "Unimplemented"
     
+@router.post("/{tenant_id}/{schema_name}")
+async def generate_sql_given_schema(tenant_id: str, schema_name: str, user_request: UserInputRequest, 
+                                    session: SessionData = Depends(authenticate_session)):
+    tenant: Tenant = await TenantManagerService.get_tenant(tenant_id=tenant_id)
+    schema: Schema = await SchemaManagerService.get_schema(tenant_id=tenant_id, schema_name=schema_name)
+    user_query_scope = await LLMServiceWrapper.get_query_scope_using_default_mode(
+        user_input=user_request
+    )
+
+    
+    query_scope_resolver = QueryScopeResolver(
+        session_data=session,
+        settings=tenant.settings,
+        query_scope=user_query_scope
+    )
+    
+    resolved_user_query_scope = query_scope_resolver.resolve_query_scope(matched_schema=schema)
+    
+    #Get ruleset from Schema, currently PoC supports only single ruleset.
+    matched_ruleset_name =  extract_ruleset_name(ruleset_placeholder=schema.filter_rules[0])
+    matched_ruleset: Ruleset = await RulesetManagerService.get_ruleset(tenant_id=tenant_id, ruleset_name=matched_ruleset_name)
+    
+    access_resolver = AccessControlResolver(session_data=session, ruleset=matched_ruleset, matched_schema=schema)
+    
+    access_resolver.has_access_to_scope(resolved_user_query_scope)
+    return resolved_user_query_scope
+    # return "Unimplemented"
